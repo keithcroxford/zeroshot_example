307bff9778843913d2126c4e78fb8519019d3be3
Keith Croxford <keithxcroxford@gmail.com>
2024-10-20 17:26:35 +0200

    adding sample code

---
+++ example.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from transformers import pipeline
+
+# define a classifier for zero shot. 
+classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
+
+# Sample ticket Data 
+ticket_data = 'example_tickets.csv'
+df = pd.read_csv(ticket_data)
+
+#Define Candidate Labels
+candidate_labels = ["Routing", "Maintenance Notices", "Password Resets", "Spam", "Web Portal Issues"]
+
+#Iterate over the Dataframe and classify
+for index, row in df.iterrows():
+    result = classifier(row['Description'], candidate_labels)
+    
+    # Convert result into a dictionary of labels and scores
+    r = dict(zip(result['labels'], result['scores']))
+    max_key = max(r, key=r.get)
+    max_value = r[max_key]
+    # Print the results 
+    print(f"Ticket #: {row['Ticket ID']} | Known Category: {row['Category']} | Zero Shot Category: {max_key} | Zero Shot Probability: {max_value}")
