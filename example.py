3d6007d334198de0577e8b88762a9a40141af958
Keith Croxford <keithxcroxford@gmail.com>
2024-10-20 17:28:08 +0200

    Update example.py

--- example.py
+++ example.py
@@ -13,11 +13,14 @@ candidate_labels = ["Routing", "Maintenance Notices", "Password Resets", "Spam",
 
 #Iterate over the Dataframe and classify
 for index, row in df.iterrows():
+    
+    # Run the classifier against the description and candidate labels
     result = classifier(row['Description'], candidate_labels)
     
     # Convert result into a dictionary of labels and scores
     r = dict(zip(result['labels'], result['scores']))
     max_key = max(r, key=r.get)
     max_value = r[max_key]
+    
     # Print the results 
     print(f"Ticket #: {row['Ticket ID']} | Known Category: {row['Category']} | Zero Shot Category: {max_key} | Zero Shot Probability: {max_value}")
